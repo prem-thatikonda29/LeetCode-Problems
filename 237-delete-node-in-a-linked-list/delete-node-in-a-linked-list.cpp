db5f96f85636d120abdd25dd33b23e6264a39548
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-05-05 20:48:21 +0530

    Time: 9 ms (35.56%) | Memory: 11.2 MB (84.10%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -9,10 +9,17 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        node->val = node->next->val;
+        // node->val = node->next->val;
 
-        ListNode* temp = node->next;
-        node->next = node->next->next;
-        delete temp;
+        // ListNode* temp = node->next;
+        // node->next = node->next->next;
+        // delete temp;
+
+        node->val = node->next->val;
+        while(node->next->next != nullptr){
+            node->next->val = node->next->next->val;
+            node = node->next;
+        }
+        node->next = NULL;
     }
 };

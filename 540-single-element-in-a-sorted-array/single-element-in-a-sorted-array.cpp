76ecc6c6a7d29432159ad2d9273e72354c955918
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-07-01 20:35:56 +0530

    Time: 22 ms (32.16%) | Memory: 24.7 MB (13.92%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -11,16 +11,16 @@ public:
 
         while(low <= high){
             int mid = (low + high) / 2;
-            if(nums[mid] == nums[mid + 1] || nums[mid] == nums[mid - 1]){
-                if((mid % 2 == 1 && nums[mid - 1] == nums[mid]) || (mid % 2 == 0 && nums[mid + 1] == nums[mid])){
+            if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid - 1]){
+                return nums[mid];
+            }
+            else{
+                if((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1])){
                     low = mid + 1;
                 }
                 else{
                     high = mid - 1;
                 }
-            }
-            else{
-                return nums[mid];
             }
         }
 

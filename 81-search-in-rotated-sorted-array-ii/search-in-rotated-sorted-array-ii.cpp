598405d92c0825d0ceea145d066e91bc69350a40
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-07-01 16:19:08 +0530

    Time: 8 ms (14.56%) | Memory: 16.5 MB (74.16%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -2,26 +2,33 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) {
         int left = 0;
-        int right = nums.size() - 1; // Corrected
+        int right = nums.size() - 1; 
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2; // Corrected for overflow
-            if (nums[mid] == target) return true; // Added handling for equal case
-            else if (nums[mid] < nums[right]) {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            } else if (nums[mid] > nums[left]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            } else {
-                if (nums[left] == target) return true; 
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) return true;
+            if(nums[left] == nums[mid] && nums[mid] == nums[right]){
                 left++;
+                right--;
+                continue;
+            }
+
+            if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && target <= nums[mid]){
+                    right = mid -1;
+                }
+                else{
+                    left = mid + 1;
+                }
+            }
+
+            else{
+                if(nums[mid] <= target && target <= nums[right]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
             }
         }
 

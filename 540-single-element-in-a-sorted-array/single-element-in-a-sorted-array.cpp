fb58a4ca04352c22bca2da168c871c06593b1767
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-07-01 20:30:20 +0530

    Time: 22 ms (32.16%) | Memory: 24.6 MB (51.59%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+
+        int low = 1;
+        int high = nums.size() - 2;
+
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[high + 1] != nums[high]) return nums[high + 1];
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == nums[mid + 1] || nums[mid] == nums[mid - 1]){
+                if((mid % 2 == 1 && nums[mid - 1] == nums[mid]) || (mid % 2 == 0 && nums[mid + 1] == nums[mid])){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid - 1;
+                }
+            }
+            else{
+                return nums[mid];
+            }
+        }
+
+        return -1;
+    }
+};

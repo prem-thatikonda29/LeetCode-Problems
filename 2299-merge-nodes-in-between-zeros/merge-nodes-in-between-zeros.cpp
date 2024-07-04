a9d38c4cb5b833ecdb3d00f04a8430af4589ac7d
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-07-04 12:11:17 +0530

    Time: 475 ms (20.28%) | Memory: 278.1 MB (18.87%) - LeetSync

--- 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
+++ 2299-merge-nodes-in-between-zeros/merge-nodes-in-between-zeros.cpp
@@ -9,25 +9,25 @@
  * };
  */
 class Solution {
-public:
-    ListNode* mergeNodes(ListNode* head) {
-    ListNode* dummy = new ListNode(0);  
-    ListNode* current = dummy;          
-    ListNode* temp = head->next;       
-    int sum = 0;
+    public:
+        ListNode* mergeNodes(ListNode* head) {
+        ListNode* dummy = new ListNode(0);  
+        ListNode* current = dummy;          
+        ListNode* temp = head->next;       
+        int sum = 0;
 
-    while (temp) {
-        if(temp->val == 0){
-            current->next = new ListNode(sum);
-            current = current->next;
-            sum = 0;
-        } 
-        else{
-            sum += temp->val;
+        while (temp) {
+            if(temp->val == 0){
+                current->next = new ListNode(sum);
+                current = current->next;
+                sum = 0;
+            } 
+            else{
+                sum += temp->val;
+            }
+            temp = temp->next;
         }
-        temp = temp->next;
-    }
 
-    return dummy->next;
-}
+        return dummy->next;
+    }
 };

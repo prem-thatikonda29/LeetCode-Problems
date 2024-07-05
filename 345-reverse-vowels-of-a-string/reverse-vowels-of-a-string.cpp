6be6b17ec5d46684e72af9535a3d1782f9dd6bb9
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-07-05 19:50:45 +0530

    Time: 5 ms (74.77%) | Memory: 9.1 MB (58.82%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -3,17 +3,18 @@ public:
     string reverseVowels(string s) {
         int start = 0, end = s.length() - 1;
         while(start < end){
-            while(start < end && !isVowel(s[start])){
+            if(isVowel(s[start]) && isVowel(s[end])){
+                swap(s[start], s[end]);
                 start++;
-            }
-
-            while(start < end && !isVowel(s[end])){
                 end--;
             }
-
-            swap(s[start], s[end]);
-            start++;
-            end--;
+            if(!isVowel(s[start])){
+                start++;
+            }
+            if(!isVowel(s[end])){
+                end--;
+                
+            }
         }
 
         return s;

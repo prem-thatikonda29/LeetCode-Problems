678037516675df6be12d44eade8cc0ec955e0401
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-07-08 13:35:35 +0530

    Time: 2 ms (75.45%) | Memory: 7.6 MB (43.98%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -6,12 +6,12 @@ public:
             friends.push_back(i + 1);
         }
 
-        int index = 0;
+        int id = 0;
         while(friends.size() > 1){
-            index = (index + k - 1) % friends.size();
-            friends.erase(friends.begin() + index);
+            id = (id + k - 1) % friends.size();
+            friends.erase(friends.begin() + id);
         }
 
         return friends[0];
     }
-};
+};             

b542dfd9545f74e873e7b333753896bfaba156e7
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-07-14 16:35:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (21.01%) - LeetSync

--- 726-number-of-atoms/number-of-atoms.cpp
+++ 726-number-of-atoms/number-of-atoms.cpp
@@ -7,73 +7,70 @@ public:
         for (int i = 0; i < n; i++) {
             char ch = formula[i];
 
-            // Handling element names
-            if (isupper(ch)) {
+            // Handle '('
+            if (ch == '(') {
+                stk.push({"(", -1});
+            }
+            // Handle elements
+            else if (isupper(ch)) {
+                int j = i + 1; // Initialize j properly
                 string element = "";
                 element += ch;
-                int j = i + 1;
                 while (j < n && islower(formula[j])) {
                     element += formula[j];
                     j++;
                 }
 
-                // Handling digits after element names
                 string sdigit = "";
                 while (j < n && isdigit(formula[j])) {
                     sdigit += formula[j];
                     j++;
                 }
+
                 if (sdigit == "") sdigit = "1";
                 int digit = stoi(sdigit);
 
                 stk.push({element, digit});
-                i = j - 1;
+                i = j - 1; // Update i properly
             }
-
-            // Handle '('
-            else if (ch == '(') {
-                stk.push({"(", -1});
-            }
-
             // Handle ')'
             else if (ch == ')') {
-                int j = i + 1;
                 string sdigit = "";
+                int j = i + 1;
                 while (j < n && isdigit(formula[j])) {
                     sdigit += formula[j];
                     j++;
                 }
+
                 if (sdigit == "") sdigit = "1";
                 int digit = stoi(sdigit);
 
                 vector<pair<string, int>> temp;
-                while (stk.top().first != "(") {
-                    auto p = stk.top();
+                while (!stk.empty() && stk.top().first != "(") {
+                    pair<string, int> p = stk.top();
                     stk.pop();
                     temp.push_back({p.first, p.second * digit});
                 }
                 stk.pop(); // Pop the '('
 
-                for (auto it = temp.rbegin(); it != temp.rend(); ++it) {
+                for (auto it = temp.rbegin(); it != temp.rend(); it++) {
                     stk.push(*it);
                 }
-                i = j - 1;
+                i = j - 1; // Update i properly
             }
         }
 
-        // Consolidate the counts
-        map<string, int> mpp;
+        map<string, int> mp;
         while (!stk.empty()) {
             auto p = stk.top();
             stk.pop();
-            mpp[p.first] += p.second;
+            mp[p.first] += p.second;
         }
 
-        // Build the result string
         string ans = "";
-        for (auto it = mpp.begin(); it != mpp.end(); ++it) {
-            ans += it->first;
-            if (it->second > 1) ans += to_string(it->second);
+        for (auto it : mp) {
+            ans += it.first;
+            if (it.second > 1) ans += to_string(it.second); // Convert integer to string
         }
 
         return ans;

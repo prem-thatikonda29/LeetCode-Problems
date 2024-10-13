bf629da8f59385f409be91057de9fd0fe9c20cd3
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-10-13 12:32:50 +0530

    Time: 31 ms (95.60%) | Memory: 45.1 MB (44.31%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -4,6 +4,7 @@ public:
         int lsum = 0, rsum = 0, ridx = cardPoints.size() - 1, maxSum = INT_MIN;
 
         for(int i = 0; i < k; i++) lsum += cardPoints[i];
+
         maxSum = lsum;
 
         for(int i = k - 1; i >= 0; i--){

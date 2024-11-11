a96d7d47bc944ac3be639c79b9201d4c92eadf2a
Prem Thatikonda <2023.tprem@isu.ac.in>
2024-11-11 20:07:04 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (78.02%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,36 +1,48 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.empty()) {
-            return {-1, -1};
-        }
-        
-        int first = -1, last = -1;
+        vector<int> ans = {-1, -1};
+        ans[0] = firstOcc(nums, target);
+        ans[1] = lastOcc(nums, target);
+        return ans;
+    }
+
+private:
+    int firstOcc(vector<int>& nums, int target) {
         int start = 0, end = nums.size() - 1;
-        
-        while(start <= end) { 
+        int ans = -1;
+
+        while (start <= end) {
             int mid = start + (end - start) / 2;
             
-            if(nums[mid] == target) {
-                first = mid;
-                last = mid;
-                
-                while(first > 0 && nums[first - 1] == target) {
-                    first--;
-                }
-                
-                while(last < nums.size() - 1 && nums[last + 1] == target) {
-                    last++;
-                }
-                
-                return {first, last};
-            } else if(nums[mid] < target) {
+            if (nums[mid] == target) {
+                ans = mid;
+                end = mid - 1; // Move left to find the first occurrence
+            } else if (nums[mid] < target) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
         }
-        
-        return {-1, -1};
+        return ans;
+    }
+
+    int lastOcc(vector<int>& nums, int target) {
+        int start = 0, end = nums.size() - 1;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            
+            if (nums[mid] == target) {
+                ans = mid;
+                start = mid + 1; // Move right to find the last occurrence
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return ans;
     }
 };

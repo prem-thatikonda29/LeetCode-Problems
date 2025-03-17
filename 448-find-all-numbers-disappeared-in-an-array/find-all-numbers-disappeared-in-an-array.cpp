deb0b4e056b9108594bb1fc0c5a11b40e53c4ed1
Prem Thatikonda <2023.tprem@isu.ac.in>
2025-03-17 18:50:35 +0530

    Time: 39 ms (36.32%) | Memory: 64.2 MB (22.82%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> present(n + 1, 0);
-
+        unordered_set<int> st;
         for(int num : nums){
-            present[num]++;
+            st.insert(num);
         }
 
-        vector<int> disappeared;
-        for(int i = 1; i <= n; i++){
-            if(present[i] == 0){
-                disappeared.push_back(i);
+        vector<int> ret;
+
+        for(int i = 1; i <= nums.size(); i++){
+            if(st.find(i) == st.end()){
+                ret.push_back(i);
             }
         }
 
-        return disappeared;
+        return ret;
     }
 };

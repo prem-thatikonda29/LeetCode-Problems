f38d0853773d6f7be84049a7df522d493fa86589
Prem Thatikonda <2023.tprem@isu.ac.in>
2025-04-14 15:16:18 +0530

    Time: 1 ms (20.91%) | Memory: 9.5 MB (19.11%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> res;
+    vector<string> codes = {
+        "", "", "abc", "def", "ghi", "jkl",
+        "mno", "pqrs", "tuv", "wxyz"
+    };
+
+    void dialPad(string str, string ans) {
+        if (str.length() == 0) {
+            res.push_back(ans);
+            return;
+        }
+
+        char ch = str[0];
+        string roq = str.substr(1);
+
+        int idx = ch - '0';
+        for (int i = 0; i < codes[idx].length(); i++) {
+            char code = codes[idx][i];
+            dialPad(roq, ans + code);
+        }
+    }
+
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return res;
+        dialPad(digits, "");
+        return res;
+    }
+};

dcabd18323e61747941355184425cc27314e3aee
Prem Thatikonda <2023.tprem@isu.ac.in>
2025-04-21 10:33:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (61.06%) - LeetSync

---
+++ 415-add-strings/add-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        string ans = "";
+        int i = num1.size() - 1;
+        int j = num2.size() - 1;
+        int carry = 0;
+
+        while(i >= 0 || j >= 0 || carry){
+            int n1 = (i >= 0) ? num1[i] - '0' : 0; 
+            int n2 = (j >= 0) ? num2[j] - '0' : 0;
+
+            int sum = n1 + n2 + carry;
+            carry = sum / 10;
+            ans += (sum % 10) + '0';
+
+            i--;
+            j--;
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};

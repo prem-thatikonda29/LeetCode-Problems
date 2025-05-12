c8a1a933f0125b4efd574b7c1514e21bbff626bc
Prem Thatikonda <2023.tprem@isu.ac.in>
2025-05-12 12:26:06 +0530

    Time: 0 ms (100.00%) | Memory: 27.5 MB (70.75%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,38 +1,30 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& grid, int r, int c){
-        int rows = grid.size();
-        int cols = grid[0].size();
-
-        vector<vector<int>> dir = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        grid[r][c] = 0;
-        int area = 1;
-
-        for (auto& d : dir) {
-            int newR = r + d[0];
-            int newC = c + d[1];
-            if(newR >= 0 && newC >= 0 && newR < rows && newC < cols && grid[newR][newC] == 1) {
-                area += dfs(grid, newR, newC);
-            }
+    int dfs(vector<vector<int>>& g, int i, int j){
+        if(i < 0 || j < 0 || i >= g.size() || j >= g[0].size() || g[i][j] == 0){
+            return 0;
         }
 
-        // area += dfs(grid, r, c - 1);
-        // area += dfs(grid, r, c + 1);
-        // area += dfs(grid, r - 1, c);
-        // area += dfs(grid, r + 1, c);
+        g[i][j] = 0;
+        int area = 1;
+        area += dfs(g, i - 1, j);
+        area += dfs(g, i + 1, j);
+        area += dfs(g, i, j - 1);
+        area += dfs(g, i, j + 1);
+
         return area;
     }
 
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        if(grid.empty()) return 0;
+        int maxArea = 0;
         int rows = grid.size();
         int cols = grid[0].size();
-        int maxArea = 0;
 
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
-                if(grid[i][j] == 1) {
-                    maxArea = max(maxArea, dfs(grid, i, j));
+                if(grid[i][j] == 1){
+                    int area = dfs(grid, i, j);
+                    maxArea = max(maxArea, area);
                 }
             }
         }

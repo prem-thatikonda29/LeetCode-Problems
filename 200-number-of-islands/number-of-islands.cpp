ec07735e63d4a6351731b301e9bc37bed2f1be92
Prem Thatikonda <2023.tprem@isu.ac.in>
2025-05-12 11:45:37 +0530

    Time: 26 ms (64.55%) | Memory: 16.3 MB (65.09%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int r, int c){
-        int rows = grid.size();
-        int cols = grid[0].size();
-
-        // if element is '0', or if the index is out of bounds, 
-        // either negative or greater than the bound size
-        if(r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] == '0'){
+    void dfs(vector<vector<char>>& g, int i, int j){
+        if(i < 0 || j < 0 || i >= g.size() || j >= g[0].size() || g[i][j] == '0'){
             return;
         }
 
-        grid[r][c] = '0';
-
-        dfs(grid, r, c - 1);
-        dfs(grid, r, c + 1);
-        dfs(grid, r - 1, c);
-        dfs(grid, r + 1, c);
+        g[i][j] = '0';
+        dfs(g, i - 1, j);
+        dfs(g, i + 1, j);
+        dfs(g, i, j - 1);
+        dfs(g, i, j + 1);
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        if(grid.empty()) return 0;
-
+        int res = 0;
         int rows = grid.size();
         int cols = grid[0].size();
-        int count = 0;
-
+        
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
                 if(grid[i][j] == '1'){
+                    res++;
                     dfs(grid, i, j);
-                    count++;
                 }
             }
         }
 
-        return count;
+        return res;
     }
 };
